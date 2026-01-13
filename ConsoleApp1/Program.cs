649504a94976e3b5b6618aadcb60e6aa9b8666b7
athuljain <athuljainkj2@gmail.com>
2026-01-13 08:20:06 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("enter your age");
+            int Age = Convert.ToInt32(Console.ReadLine());
+            if (Age >= 18)
+            {
+                Console.WriteLine("you are eligible to vote");
+            }
+            else
+            {
+                Console.WriteLine("you are not eligible to vote");
+            }
+        }
+    }
+}
