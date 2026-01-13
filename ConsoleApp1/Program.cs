4fa514b99f23acef343c3bfa05d07fd7d82e2990
athuljain <athuljainkj2@gmail.com>
2026-01-13 08:51:03 +0530

    method

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,21 +1,55 @@
-﻿using System;
+﻿//using System;
 
-namespace Program
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("enter your age");
-            int Age = Convert.ToInt32(Console.ReadLine());
-            if (Age >= 18)
-            {
-                Console.WriteLine("you are eligible to vote");
-            }
-            else
-            {
-                Console.WriteLine("you are not eligible to vote");
-            }
-        }
-    }
-}
+//namespace Program
+//{
+//    public class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            Console.WriteLine("enter your age");
+//            int Age = Convert.ToInt32(Console.ReadLine());
+//            if (Age >= 18)
+//            {
+//                Console.WriteLine("you are eligible to vote");
+//            }
+//            else
+//            {
+//                Console.WriteLine("you are not eligible to vote");
+//            }
+//        }
+//    }
+//}
+
+
+//------------------------------------------------------------------
+
+
+// method  
+
+//----------------------------------------------------------------
+
+//using System;
+
+//namespace Program
+//{
+//    public class Program
+//    {
+
+//        public static int LargestNumber(int num1, int num2)
+//        {
+//            int result;
+//            if(num1>num2){
+//                result=num1;
+//        }else{
+//            result=num2;
+//        } 
+//            return result;
+//        }
+
+
+//        public static void Main(String[] args)
+//        {
+//    Console.WriteLine(LargestNumber(12, 23));
+//        }
+//    }
+//}
