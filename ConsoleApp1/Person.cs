3e04b61a74817456e535bf0814d9615fd8335c50
athuljain <athuljainkj2@gmail.com>
2026-01-14 11:08:20 +0530

    class

---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Program
+{
+    public class Person
+    {
+        //fields
+        public string Name;
+        public int Age;
+
+        public void Display()
+        {
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Age: " + Age);
+        }
+
+    }
+
+
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -53,3 +53,49 @@
 //        }
 //    }
 //}
+
+
+
+//===============================================================
+
+//namespace Program
+//{
+//    public class Program
+//    {
+//        public static int addNum(int x ,int y)
+//        {
+//            return x + y;
+//        }
+
+//        public static double addNum(double x, double y)
+//        {
+//            return x + y;
+//        }
+//        public static void Main(string[] args)
+//        {
+//            Console.WriteLine(addNum(5, 10));
+//            Console.WriteLine(addNum(5.6, 10));
+//        }
+//    }
+//}
+
+
+//===============================================================
+
+
+using System;
+
+namespace Program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.Name = "John";
+            p1.Age = 30;
+            p1.Display();
+
+        }
+    }
+}
